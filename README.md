10c3ce3c6d62fe02be30e9b5031a08c85be14179
tp <tp@octavore.com>
2014-02-12 21:22:45 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Heuristics
+
+Heuristics for determining what static site generator gets executed in Pancake.io when a git push is received.
---
+++ heuristics.go
@@ -0,0 +1,46 @@
+// heuristics determines how projects are built in git-based Pancake.io projects.
+package heuristics
+
+type StaticSiteType struct {
+	Name    string
+	Canary  string
+	Command string
+}
+
+var DefaultBuild = &StaticSiteType{
+	Name:    "html",
+	Command: "cp -vXR $PANCAKE_SOURCE/* $PANCAKE_ARTIFACT_DIR",
+}
+
+var StaticSites = map[string]*StaticSiteType{
+	"jekyll": {
+		Name:    "jekyll",
+		Canary:  "_config.yml",
+		Command: "touch Gemfile && bundle install && jekyll build --source $PANCAKE_SOURCE --destination $PANCAKE_ARTIFACT_DIR",
+	},
+	"pelican": {
+		Name:    "pelican",
+		Canary:  "pelicanconf.py",
+		Command: "pelican $PANCAKE_SOURCE --output $PANCAKE_ARTIFACT_DIR --verbose",
+	},
+	"wintersmith": {
+		Name:    "wintersmith",
+		Canary:  "config.json",
+		Command: "npm install && wintersmith build -C $PANCAKE_SOURCE -o $PANCAKE_ARTIFACT_DIR",
+	},
+	"middleman": {
+		Name:    "middleman",
+		Canary:  "config.rb",
+		Command: "middleman build && cp -r $PANCAKE_SOURCE/build/* $PANCAKE_ARTIFACT_DIR/",
+	},
+	"hyde": {
+		Name:    "hyde",
+		Canary:  "info.yaml",
+		Command: "hyde gen -s $PANCAKE_SOURCE -d $PANCAKE_ARTIFACT_DIR",
+	},
+	"sphinx": {
+		Name:    "sphinx",
+		Canary:  "conf.py",
+		Command: "sphinx-build -b html $PANCAKE_SOURCE $PANCAKE_ARTIFACT_DIR",
+	},
+}
